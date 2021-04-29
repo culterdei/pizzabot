e0deaa47745c7e31dbd6edfc56b880b6540708d0
culterdei <49120990+culterdei@users.noreply.github.com>
2021-04-29 11:27:30 +0300

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -9,12 +9,14 @@ def move_pizza(input_string:str):
     mov_seq = []
     for point in points:
         point = literal_eval(point)
+        # X axis
         if current_point[0] > point[0]:
             mov_seq.append('W'*(current_point[0] - point[0]))
         elif current_point[0] < point[0]:
             mov_seq.append('E'*(point[0] - current_point[0]))
         else:
             pass
+        # Y axis
         if current_point[1] > point[1]:
             mov_seq.append('S'*(current_point[1] - point[1]))
         elif current_point[1] < point[1]:
@@ -25,23 +27,24 @@ def move_pizza(input_string:str):
         mov_seq.append('D')
     return ''.join(mov_seq)
 
-# Mapping to function
-cmd = {'pizzabot': move_pizza}
-
-# Pizzabot arguments count check
-if len(sys.argv) < 2:
-    print('Not enought arguments')
-    sys.exit()
-
-# Pizzabot command check
-if sys.argv[1] != 'pizzabot':
-    print(f'It"s {sys.argv[1]}, not an pizzabot')
-    sys.exit()
-
-# Pizzabot arguments validation through regex
-if not re.search('^\d{1,}x\d{1,}([ ]?\(\d{1,},\d{1,}\)[ ]?){1,}$', sys.argv[2]):
-    print('Invalid pizzabot arguments - please, provide with following pattern: _x_ (_,_) (_,_) (_,_)...')
-    sys.exit()
-
 if __name__ == '__main__':
+    
+    # Mapping to function
+    cmd = {'pizzabot': move_pizza}
+
+    # Pizzabot arguments count check
+    if len(sys.argv) < 2:
+        print('Not enought arguments')
+        sys.exit()
+
+    # Pizzabot command check
+    if sys.argv[1] != 'pizzabot':
+        print(f'It"s {sys.argv[1]}, not an pizzabot')
+        sys.exit()
+
+    # Pizzabot arguments validation through regex
+    if not re.search('^\d{1,}x\d{1,}([ ]?\(\d{1,},\d{1,}\)[ ]?){1,}$', sys.argv[2]):
+        print('Invalid pizzabot arguments - please, provide with following pattern: _x_ (_,_) (_,_) (_,_)...')
+        sys.exit()
+    
     print(cmd[sys.argv[1]](sys.argv[2]))
